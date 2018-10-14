d2e65e90a354a3c687770f90c00011b0a20aab3c
Li Haiyue <johnlee1029521@gmail.com>
2018-10-14 18:21:10 +1100

    Polished Server.java
    Update Server.java with comments and helper functions, testing passed.

--- DistributedScramble/src/ServerSide/Server.java
+++ DistributedScramble/src/ServerSide/Server.java
@@ -1,14 +1,12 @@
-
-
 /******************************************************************************
  *  Dependencies: gson-2.8.5.jar (third-party library) 
  *                Protocol Package
  *                ServeClientThread.java
  *
- *  The main thread of ServerSide programs. 
+ *  The main thread on Server side of Scrabble Game System . 
  *  It initializes Server socket, keeps listening to client's connection and creates threads for each incoming connection.
- *  It also implements several broadcasting functions like broadcasting waitingList.
- *  It keeps clientThreads by ConcurrentHashMap. It also keeps waitingList and gameList.
+ *  It also implements several broadcasting functions.
+ *  It keeps system relevant data structures such as clientThreads by ConcurrentHashMap.
  *  
  ******************************************************************************/
 
@@ -36,28 +34,45 @@ public class Server {
     private int port;   // Server listening port, parsed by cmdLineArgs.Class
     private ConcurrentHashMap<String, ServeClientThread> threadMap; // HashMap takes player's username and corresponding Thread as key value pair.
     private ServerSocket listeningSocket;   // The server socket bounded to specified port indicated above.
-    private List<String> waitingList;  // The list keeps usernames of players who are waiting to play.
-    private List<String> gameList;  // The list keeps usernames of players who are waiting to play.
-    private String gameStarted;
+    private List<String> waitingList;  // The list keeps usernames of players who are waiting in waiting room.
+    private List<String> gameList;  // The list keeps usernames of players who are waiting in game room.
+    private String gameCreated;
     private int chance;
     private int passCount;
     
+    /**
+     * The Server Class takes a port number as parameter.
+     * It starts a ServerSocket bounds to that port and keeps listening to client connection.
+     * 
+     * @param port          The port number Server keeps listening to.
+     * @throws IOException  throws IOException when ServerSocket cannot be created.
+     */
     public Server(int port) throws IOException {
         this.port = port;
         listeningSocket = new ServerSocket(port);
         threadMap = new ConcurrentHashMap<String, ServeClientThread>();
         waitingList = Collections.synchronizedList(new ArrayList<String>());
         gameList = Collections.synchronizedList(new ArrayList<String>());
-        gameStarted="No";
-        this.chance=0;
-        this.passCount=0;
+        gameCreated = "No";
+        this.chance = 0;
+        this.passCount = 0;
     }
-
+    
+    /**
+     * ServerSocket starts to keep listening on given port number. 
+     * Once a clientSocket comes in, prints out its brief information, 
+     * creates a corresponding ServeClientThread to serve client and submit it to ThreadPool.
+     * 
+     * @param scanner   the keyboard scanner on server side, used to input monitoring commands.
+     * @param executor  the ThreadPool to which ServeClientThreads are submitted.
+     */
     public void execute(Scanner scanner, ExecutorService executor) {
         try {
             System.out.println("Server listening on port " + port + " for a connection");
             int count = 0;
             
+            // ControlThread controlThread = new ControlThread(scanner, this, this.listeningSocket, executor);
+            // controlThread.start();
             while (true) {
                 Socket clientSocket = listeningSocket.accept();
                 count++;
@@ -65,7 +80,7 @@ public class Server {
                 System.out.println("Remote Port: " + clientSocket.getPort());
                 System.out.println("Remote Hostname: " + clientSocket.getInetAddress().getHostName());
                 System.out.println("Local Port: " + clientSocket.getLocalPort());
-                ServeClientThread t = new ServeClientThread(clientSocket, this,gameStarted);
+                ServeClientThread t = new ServeClientThread(clientSocket, this, gameCreated);
                 executor.submit(t);
             }
         } catch (UnsupportedEncodingException e) {
@@ -83,127 +98,178 @@ public class Server {
         }
     }
 
+    /**
+     * Broadcasting a message to all clients have logged in.
+     * 
+     * @param packet    the Packet object to be broadcast
+     */
     public void broadcast(Packet<?> packet) {
         for (ServeClientThread t : threadMap.values()) {
             t.send(packet);
         }
     }
 
+    /**
+     * Broadcasting username list waiting in waiting room.
+     * 
+     */
     public void broadcastWaitingList() {
         String[] list = waitingList.toArray(new String[0]);
-        
-        if(list.length==0) {
-        	if(threadMap.isEmpty()) {
-        		
-        	}
-        	else {
-        		for (ServeClientThread t : threadMap.values())
-        			t.send(new Packet<WaitingList>("WaitingList", null,"Server"));
-        			}
-        	}
-        	else {
-        	for (ServeClientThread t : threadMap.values()) 
-        		t.send(new Packet<WaitingList>("WaitingList", new WaitingList(list,gameStarted),"Server"));
-        	}
+
+        if (list.length != 0) {
+            for (ServeClientThread t : threadMap.values())
+                t.send(new Packet<WaitingList>("WaitingList", new WaitingList(list, gameCreated), "Server"));
+        } else {
+            if (!threadMap.isEmpty()) {
+                for (ServeClientThread t : threadMap.values())
+                    t.send(new Packet<WaitingList>("WaitingList", null, "Server"));
+            }
         }
-    
-    public void resetChance()
-    {
-    	this.chance=0;
     }
     
+    /**
+     * Broadcasting username list waiting in game room.
+     * 
+     */
     public void broadcastGameList() {
         String[] list = gameList.toArray(new String[0]);
-        
-        //for (ServeClientThread t : threadMap.values()) {
-        for (String name: gameList ) {
-        	ServeClientThread t=threadMap.get(name);
-            t.send(new Packet<GameList>("GameList", new GameList(list),"Server"));
+
+        for (String name : gameList) {
+            ServeClientThread t = threadMap.get(name);
+            t.send(new Packet<GameList>("GameList", new GameList(list), "Server"));
         }
     }
     
-    public void invitePlayers(String[] invitation_list,String invitee) {
-    	for (String name: invitation_list) {
-    		threadMap.get(name).send(new Packet<Invitation>("Invitation", new Invitation(invitee),"Server"));
-    	}
+    /**
+     * Send Invitation message to players are invited.
+     * 
+     * @param invitation_list   the list of invited users' username.
+     * @param invitee   the username of player who initiates invitation request.
+     */
+    public void invitePlayers(String[] invitation_list, String invitee) {
+        for (String name : invitation_list) {
+            threadMap.get(name).send(new Packet<Invitation>("Invitation", new Invitation(invitee), "Server"));
+        }
     }
 
+    /**
+     * Attempts to register a username on Server, returns registration result in boolean type.
+     * 
+     * @param username  the username to be registered
+     * @param thread    the corresponding ServeClientThread
+     * @return  true if registered successfully;
+     *          false if username has existed or username is illegal.
+     */
     public synchronized Boolean registerToWaiting(String username, ServeClientThread thread) {
-
-    	if(threadMap.containsKey(username) || username == null || username.contains(" ") || !username.matches("[A-Za-z0-9]+")) {
-    		return false;
-    	}
-    	else {
-	        threadMap.put(username, thread);
-	        waitingList.add(username);
-	        return true;
-    	}
+        if (threadMap.containsKey(username) || !isUsernameValidate(username)) {
+            return false;
+        } else {
+            threadMap.put(username, thread);
+            waitingList.add(username);
+            return true;
+        }
     }
     
+    private boolean isUsernameValidate(String username) {
+        return username != null && !username.contains(" ") && username.matches("[A-Za-z0-9]+");
+    }
+    
+    /**
+     * Register a username from waiting list to game list.
+     * 
+     * @param username  the username to be registered
+     */
     public synchronized void registerToGame(String username) {
         gameList.add(username);
         waitingList.remove(username);
-        
     }
     
-    public void broadcastWord(Packet<Insert> packet ) {
-        
-        //for (ServeClientThread t : threadMap.values()) {
-        for (String name: gameList ) {
-        	ServeClientThread t=threadMap.get(name);
+    /**
+     * Broadcasting highlighted word to players in game.
+     * 
+     * @param packet    the packet object containing the word
+     */
+    public void broadcastWord(Packet<Insert> packet) {
+        for (String name : gameList) {
+            ServeClientThread t = threadMap.get(name);
             t.send(packet);
         }
     }
     
+    /**
+     * Send end game message to all players in the game.
+     * 
+     */
     public synchronized void endGame() {
-    	System.out.println("GameList Size:"+gameList.size());
-    	for (String name: gameList ) {
-        	ServeClientThread t=threadMap.get(name);
-        	System.out.println("Sending End Game Packet to:"+name);
+        System.out.println("GameList Size:" + gameList.size());
+        for (String name : gameList) {
+            ServeClientThread t = threadMap.get(name);
+            System.out.println("Sending End Game Packet to:" + name);
             t.send(new Packet<GameList>("EndGame", null, "Server"));
-            System.out.print("Sent End Game Packet to:"+name);
+            System.out.print("Sent End Game Packet to:" + name);
             waitingList.add(name);
-            
         }
-    	updateGameStatus("No");
-    	broadcastWaitingList();
-    	gameList.clear();
-    	
+        updateGameStatus("No");
+        broadcastWaitingList();
+        gameList.clear();
     }
     
-    public void logOut(String usrnm) {
-    	waitingList.remove(usrnm);
-    	threadMap.get(usrnm).send(new Packet<GameList>("Logout", null, "Server"));
-    	threadMap.remove(usrnm);
-    	broadcastWaitingList();
+    /**
+     * When a user logs out, we remove its username from waiting together with corresponding entry in threadMap,
+     * then we broadcast the update of game list to all players.
+     * 
+     * @param username  the username of user logged out
+     */
+    public void logOut(String username) {
+        waitingList.remove(username);
+        threadMap.get(username).send(new Packet<GameList>("Logout", null, "Server"));
+        threadMap.remove(username);
+        broadcastWaitingList();
     }
     
-    public void updateGameStatus(String status)
-    {
-    	this.gameStarted=status;
+    /**
+     * Update the status of the game.
+     * @param status    the status of game, 'Yes' means already exists, 'No' means not.
+     */
+    public void updateGameStatus(String status) {
+        this.gameCreated = status;
     }
     
     public void nextChance(int i) {
-    	if(i==1)
-    		this.passCount+=1;
-    	else
-    		this.passCount=0;
-    	
-    	if(this.passCount==gameList.size())
-    		endGame();
-    	else {
-	    	if(this.chance==gameList.size())
-	    		this.chance=0;
-	    	 
-	    	String username=gameList.get(chance);
-	    	for (ServeClientThread t:threadMap.values())
-	    		t.send(new Packet<Reply>("passChance" , new Reply(Integer.toString(this.chance), true, null) , username));
-	    	this.chance+=1;
-    	}
+        if (i == 1)
+            this.passCount += 1;
+        else
+            this.passCount = 0;
+
+        if (this.passCount == gameList.size())
+            endGame();
+        else {
+            if (this.chance == gameList.size())
+                this.chance = 0;
+
+            String username = gameList.get(chance);
+            for (ServeClientThread t : threadMap.values())
+                t.send(new Packet<Reply>("passChance", new Reply(Integer.toString(this.chance), true, null), username));
+            this.chance += 1;
+        }
+    }
+    
+    /*
+    public void stopAllThreads() {
+        for (ServeClientThread t : threadMap.values()) {
+            t.close();
+        }
+    }
+    */
+    
+    public void resetChance() {
+        this.chance = 0;
     }
 
     public static void main(String[] args) {
-        int port = 0;
+        int port = 0; // default port number
+
+        // Receives listening port number from command line
         CmdLineArgs argsBean = new CmdLineArgs();
         CmdLineParser parser = new CmdLineParser(argsBean);
         try {
@@ -215,8 +281,9 @@ public class Server {
             System.exit(1);
         }
 
+        final int NUMBER_OF_THREADS_IN_POOL = 100;
         Scanner scanner = new Scanner(System.in);
-        ExecutorService executor = Executors.newFixedThreadPool(100);
+        ExecutorService executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS_IN_POOL);
         try {
             Server server = new Server(port);
             server.execute(scanner, executor);
@@ -225,4 +292,6 @@ public class Server {
         }
     }
 }
+
+
 
